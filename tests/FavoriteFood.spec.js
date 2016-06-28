0178beccf0b3b3ad0e59d6ad3f6e6b3ec7c3b022
Tyler Smith <oats000@hotmail.com>
2016-06-28 22:52:33 +0000

    Done.

--- tests/FavoriteFood.spec.js
+++ tests/FavoriteFood.spec.js
@@ -1,11 +1,21 @@
 describe('Favorite Food Filter', function () {
-	var $controller;
+	var $controller, favoriteFood;
+	var people = [
+		{
+			'name': 'Tyler',
+			'favoriteFood': 'Cheese'
+		},
+		{
+			'name': 'Jesse',
+			'favoriteFood': 'Beer'
+		},
+	];
 
-	beforeEach(module('app'));
-
-	beforeEach(inject(function ($injector) {
-		$filter = $injector.get('$filter');
+	beforeEach(inject(function ($filter) {
+			favoriteFood = $filter('favoriteFood');
 	}));
 
-	
+	it('should return array of people matching favorite food', function () {
+			expect(favoriteFood(people, 'Cheese')).toEqual([{'name': 'Tyler','favoriteFood': 'Cheese'},]);
+	});
 });
--- tests/removeAllVowels.spec.js
+++ tests/removeAllVowels.spec.js
@@ -1,10 +1,12 @@
 describe('removeAllVowels Filter', function () {
-	var $controller;
+	var $controller, removeAllVowels;
 
-	beforeEach(module('app'));
 
-	beforeEach(inject(function ($injector) {
-		$filter = $injector.get('$filter');
+	beforeEach(inject(function ($filter) {
+			removeAllVowels = $filter('removeAllVowels');
 	}));
 
+	it('should return a string without lowercase vowels', function () {
+			expect(removeAllVowels('Cheese').toEqual('Chs');
+	});
 });
